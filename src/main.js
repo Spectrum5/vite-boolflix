d6da70624fd793acf9e5190fc28482870cae0c4f
Spectrum5 <toskusergio@gmail.com>
2023-02-09 00:24:36 +0100

    Installato Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
+/* Set up using Vue 3 */
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faStar as fasStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fasStar, farStar)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
