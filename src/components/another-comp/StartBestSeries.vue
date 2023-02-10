d79738382dd57965c4ad9c5328103bfd9860bcec
Spectrum5 <toskusergio@gmail.com>
2023-02-10 19:43:28 +0100

    Refactoring StartBestSeries

--- src/components/another-comp/StartBestSeries.vue
+++ src/components/another-comp/StartBestSeries.vue
@@ -19,63 +19,37 @@ export default {
     <div v-if="store.active == false">
         <h2>Serie tv uscite di recente</h2>
         <div class="wrapper">
+
             <section id="section4">
                 <a href="#section6" class="arrow__btn">‹</a>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[0]?.poster_path}`" alt="">
+
+                <div class="item" v-for="index in 5">
+                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index]?.poster_path}`" alt="">
                 </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[1]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[2]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[3]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[4]?.poster_path}`" alt="">
-                </div>
+
                 <a href="#section5" class="arrow__btn">›</a>
             </section>
+
             <section id="section5">
                 <a href="#section1" class="arrow__btn">‹</a>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[5]?.poster_path}`" alt="">
+
+                <div class="item" v-for="index in 5">
+                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index + 5]?.poster_path}`" alt="">
                 </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[6]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[7]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[8]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[9]?.poster_path}`" alt="">
-                </div>
+
                 <a href="#section6" class="arrow__btn">›</a>
             </section>
+
             <section id="section6">
                 <a href="#section4" class="arrow__btn">‹</a>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[10]?.poster_path}`" alt="">
+
+                <div class="item" v-for="index in 5">
+                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[index + 10]?.poster_path}`" alt="">
                 </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[11]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[12]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[13]?.poster_path}`" alt="">
-                </div>
-                <div class="item">
-                    <img :src="`http://image.tmdb.org/t/p/w500/${seriesList[14]?.poster_path}`" alt="">
-                </div>
+
                 <a href="#section4" class="arrow__btn">›</a>
             </section>
+
         </div>
     </div>
 
